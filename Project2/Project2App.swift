cac10fed79142de19e749046964d7998efc2a9d8
AUDTJ44 <mptyshot0@gmail.com>
2023-09-27 15:04:51 +0900

    Initial Commit

---
+++ Project2/Project2App.swift
@@ -0,0 +1,17 @@
+//
+//  Project2App.swift
+//  Project2
+//
+//  Created by 이명서 on 2023/09/27.
+//
+
+import SwiftUI
+
+@main
+struct Project2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
